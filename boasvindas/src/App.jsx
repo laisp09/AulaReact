4a09373941c177f4f963813cced71f612b831faa
laisp09 <laisperpetuosilva@gmail.com>
2025-04-08 12:24:06 -0300

    Aula 0804

---
+++ boasvindas/src/App.jsx
@@ -0,0 +1,25 @@
+import BoasVindas from "./Exemplo1"
+
+function App() {
+  
+  return (
+   <div>
+
+    <h1> Boas Vindas ao React com JavaScript </h1>
+
+   <p>
+     Este é o arquivo App.jsx que será o nosso ponto de partida para o desenvolvimento
+     de aplicativo de aplicação React utilizando JavaScript.
+   </p>
+
+   <BoasVindas nome="Laís" />
+   <BoasVindas nome="Maria Fernanda" />
+   <BoasVindas nome="Lorena" />
+   <BoasVindas nome="Anna Luiza" />
+   <BoasVindas nome="jorge amor" />
+
+   </div>
+  )
+}
+
+export default App
