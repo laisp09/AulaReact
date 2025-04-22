18084efb6deac10208ca8158d2d2e3d4a8d63011
laisp09 <laisperpetuosilva@gmail.com>
2025-04-22 12:22:35 -0300

    22/04
    22/04

---
+++ exercicioscomponentes/src/Componentes/Exercicio3.jsx
@@ -0,0 +1,12 @@
+export default function Exercicio3({ valor, taxa, tempo})
+{
+    let valorparcela = valor + (valor * (taxa*taxa/100) * tempo)
+    return (
+        <div>
+        Valor: R${valor} <br/>
+        Taxa: {taxa}% <br/>
+        Tempo: {tempo} dias <br/>
+        Valor da parcela em atraso é {valorparcela}
+        </div>
+     )
+}
