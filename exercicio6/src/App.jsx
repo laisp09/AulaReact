2a5b420e167d9da0d57742eafe7ed3728e0e5d0e
laisp09 <laisperpetuosilva@gmail.com>
2025-05-20 12:27:52 -0300

    20/05
    20/05

---
+++ exercicio6/src/App.jsx
@@ -0,0 +1,12 @@
+import Exercicio6 from "./Exercicio6"
+import Exercicio7 from "./Exercicio7"
+
+export default function App()
+{
+  return (
+    <div>
+      <h1>Exercicio 6</h1>
+      <Exercicio6 quantidade={2} preco={5}/>
+    </div>
+  );
+}
---
+++ exercicio6/src/Exercicio6.jsx
@@ -0,0 +1,13 @@
+export default function Exercicio6({ quantidade, preco})
+{
+    let subtotal = (quantidade*preco);
+    let desconto = (subtotal*0.1)
+    let valorf = (subtotal-desconto)
+    return (
+        <div>
+          Total: R${subtotal} <br/>
+          Desconto: R${desconto} <br/>
+          Valor Final: R${valorf} <br/>
+        </div>
+     );
+}
