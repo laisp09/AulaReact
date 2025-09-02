16e29bdad96cf8f58899c83c61555fb6976c1d26
laisp09 <laisperpetuosilva@gmail.com>
2025-09-02 12:09:54 -0300

    02/09

---
+++ estudoestados/src/Paginas/Exercicio2.jsx
@@ -0,0 +1,11 @@
+export default function Exercicio2() {
+    return(
+      <div>
+         <h1>Exercício 2 </h1>
+  
+  
+  
+      </div>
+  
+      );
+  }
---
+++ estudoestados/src/Paginas/Exercicio5.jsx
@@ -0,0 +1,11 @@
+export default function Exercicio5() {
+    return(
+      <div>
+         <h1>Exercício 5 </h1>
+  
+  
+  
+      </div>
+  
+      );
+  }
